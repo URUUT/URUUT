eb3305d01a6335d0978bd3075fba00050bb3689a
Chad Bartels <chad.bartels@gmail.com>
2012-07-28 13:40:56 -0400

    Precompiled for Heroku

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Crowdfund
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    config.assets.initialize_on_precompile = false
     config.assets.precompile += %w( active_admin.css.scss )
   end
 end
