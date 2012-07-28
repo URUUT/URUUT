92762f36d5d0de3f4a505268993345e497974929
Chad Bartels <chad.bartels@gmail.com>
2012-07-28 02:05:47 -0400

    Added Heroku

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'paperclip', '~>3.1.3'
 gem 'haml'
 gem "twitter-bootstrap-rails"
 gem 'devise'
+gem 'heroku'
 
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Crowdfund::Application.routes.draw do
 
   ActiveAdmin.routes(self)
 
+  root to: "projects#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,7 +56,6 @@ Crowdfund::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
