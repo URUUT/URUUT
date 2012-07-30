3893ce0d7711c712167b7b22b37d22f2a5dfbf14
Chad Bartels <chad.bartels@gmail.com>
2012-07-30 03:49:27 -0400

    Still working on it.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,18 @@
 class Project < ActiveRecord::Base
   attr_accessible :amount, :due_date, :name, :image
   
-  has_attached_file :image, 
-  	:styles => { 
-  		:large => "640x480>", 
-  		:medium => "200x150>", 
-  		:thumb => "100x100>" }
+  # has_attached_file :image, 
+  # 	:styles => { 
+  # 		:large => "640x480>", 
+  # 		:medium => "200x150>", 
+  # 		:thumb => "100x100>" }
+
+  has_attached_file :image,
+     :storage => :s3,
+     :s3_credentials => "#{::Rails.root.to_s}/config/s3.yml",
+     :s3_headers => {"Content-Disposition" => "attachment"},
+     :styles => { 
+		:large => "640x480>", 
+		:medium => "200x150>", 
+		:thumb => "100x100>" }
 end
