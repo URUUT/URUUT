9daac5638ae39a7cc8b2798cdb0863f0a7196aec
Chad Bartels <chad@chads-macbook-pro.local>
2012-08-13 01:52:24 -0400

    got multi-step form working, need to save

--- app/assets/javascripts/projects.js.coffee
+++ app/assets/javascripts/projects.js.coffee
@@ -1,3 +1,18 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$ ->
+
+	$('#step-1 .btn').click ->
+		$('#step-1').hide()
+		$('#step-2').show()
+
+	$('#step-2 .btn.next').click ->
+		$('#step-2').hide()
+		$('#step-3').show()
+		$('#new_project input[type=submit]').show()
+
+	$('#step-2 .btn.back').click ->
+		$('#step-2').hide()
+		$('#step-1').show()
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,37 +5,18 @@ class ProjectsController < ApplicationController
 	end
 
 	def new
-		session[:project_params] ||= {}
 		@project = Project.new
 		# @project.images.build
 		# @project.videos.build
-		# respond_to do |format|
-	 #      format.html # new.html.erb
-	      # format.xml  { render :xml => @project }
-	    # end
+		respond_to do |format|
+	      format.html # new.html.erb
+	      format.xml  { render :xml => @project }
+	    end
 	end
 
 	def create
-		session[:project_params].deep_merge!(params[:project]) if params[:project]
-		@project = Project.new(session[:project_params])
-		@project.current_step = session[:project_step]
-
-		if params[:back_button]
-			@project.previous_step
-		elsif @project.last_step?
-			@project.save
-			# @project.images.build
-		else
-			params[:project][:image] = nil
-			@project.next_step
-		end
-		session[:project_step] = @project.current_step
-		if @project.new_record?
-			render 'new'
-		else
-
-			session[:project_step] = session[:project_params] = nil
-			params[:project][:image] = nil rescue nil
+		@project = Project.new(params[:project])
+		if @project.save
 			redirect_to @project
 		end
 	end
