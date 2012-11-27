aa753239a921c1b05dc8cf1de84f09588340b0ae
Chad Bartels <chad.bartels@gmail.com>
2012-11-27 01:07:27 -0500

    Working on some styling

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,12 +8,12 @@ class Project < ActiveRecord::Base
   #accepts_nested_attributes_for :objectives, :allow_destroy => true
 
   has_attached_file :image
-    :styles => {
-      :thumb=> "100x100#",
-      :medium=> "280x124#" }
-     :storage => :file,
+    #:styles => {
+      #:thumb=> "100x100#",
+      #:medium=> "280x124#" }
+     #:storage => :file,
      #:s3_credentials => "#{Rails.root}/config/s3.yml",
-     :path => "/public/images/:id/:style/:filename"
+     #:path => "/public/images/:id/:style/:filename"
 
   #process_in_background :image
 
