ce100287d7079005154f6ed7f5596271b5bc7365
Chad Bartels <chad.bartels@gmail.com>
2012-11-29 19:57:24 -0500

    changed login pages for sponsors and donors

--- db/migrate/20121128053817_drop_table_perks.rb
+++
@@ -1,5 +0,0 @@
-class DropTablePerks < ActiveRecord::Migration
-  def up
-    drop_table :perks
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127213460) do
+ActiveRecord::Schema.define(:version => 20121128054418) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -64,9 +64,10 @@ ActiveRecord::Schema.define(:version => 20121127213460) do
   create_table "perks", :force => true do |t|
     t.string   "name"
     t.float    "amount"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "project_id"
   end
 
   create_table "projects", :force => true do |t|
