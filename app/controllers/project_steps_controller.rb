e51091c7a2fe75fd12b1b534b1973d70ab841ff6
Chad Bartels <chad.bartels@gmail.com>
2013-01-17 12:42:28 -0500

    Got all steps of wizard working along with edit path, just need to get first step of wizard to work right so step 1 isn't repeated.

--- app/controllers/project_steps_controller.rb
+++ app/controllers/project_steps_controller.rb
@@ -2,9 +2,16 @@ class ProjectStepsController < ApplicationController
   include Wicked::Wizard
   steps :step1, :step2, :step3, :step4
 
+  # def edit
+  #     @project = Project.find(params[:id])
+  #     render_wizard
+  #   end
+  
   def show
     @project = Project.find_by_id(session[:current_project])
     logger.debug(@project)
+    @project.perks.build
+    @project.galleries.build
     render_wizard
   end
 
