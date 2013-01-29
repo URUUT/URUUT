c2f22a2b2f93c4ae477d092a0aff7fad78c0f827
Chad Bartels <chad.bartels@gmail.com>
2013-01-29 10:46:17 -0500

    Implementing User sign up/log in designs from Nick

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Crowdfund::Application.routes.draw do
   
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  devise_for :sponsors, :controllers => { :dashboard => "sponsors/dashboard" }
   
   get "image/new"
 
@@ -24,22 +23,22 @@ Crowdfund::Application.routes.draw do
   get "pages/about"
 
   get "projects/add_desc"
- 
-  get "projects/step1"
-  post "projects/step1"
-  put "projects/step1"
-  
-  get "projects/step2"
-  post "projects/step2"
-  put "projects/step2"
-  
-  get "projects/step3"
-  post "projects/step3"
-  put "projects/step3"
-  
-  get "projects/step4"
-  post "projects/step4"
-  put "projects/step4"
+  #  
+  # get "projects/step1"
+  # post "projects/step1"
+  # put "projects/step1"
+  # 
+  # get "projects/step2"
+  # post "projects/step2"
+  # put "projects/step2"
+  # 
+  # get "projects/step3"
+  # post "projects/step3"
+  # put "projects/step3"
+  # 
+  # get "projects/step4"
+  # post "projects/step4"
+  # put "projects/step4"
   
   get "projects/stripe_update"
 
@@ -57,59 +56,4 @@ Crowdfund::Application.routes.draw do
 
   root to: "pages#home"
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
