8dddbf3869fb0116d0f6fe9993a23a6a80165459
Chad Bartels <chad.bartels@gmail.com>
2013-04-24 02:12:03 -0400

    Fixed omniauth redirect issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     logger.debug(request.referrer)
-    return request.referrer
+    return root_url
   end
 
   protected
