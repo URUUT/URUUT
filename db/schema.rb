990f364eedcf9b601f76c1292ccc61913c29c28b
Dmitry Gritsay <tbizmy@gmail.com>
2013-04-28 01:17:28 +0300

    Add public profile image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130427185440) do
+ActiveRecord::Schema.define(:version => 20130427214521) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -157,6 +157,7 @@ ActiveRecord::Schema.define(:version => 20130427185440) do
     t.string   "organization"
     t.text     "mission"
     t.boolean  "subscribed"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
