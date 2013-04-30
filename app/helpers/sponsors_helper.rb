401db39051949f0701ed26232b76ad22036b5fa0
hendra <hendra@41studio.com>
2013-04-30 20:28:36 +0700

    fixed validation

--- app/helpers/sponsors_helper.rb
+++ app/helpers/sponsors_helper.rb
@@ -3,13 +3,17 @@ module SponsorsHelper
   def sponsor_option(id)
     sponsorship_levels_active = []
     project = Project.find(id)
-    level_group = project.project_sponsors.count(group: "level_id")
+    level_groups = project.project_sponsors.count(group: "level_id")
+    levels = project.sponsorship_levels.pluck(:id)
 
-    level_group.each do |key, value|
+    levels.each { |level| level_groups[level] = 0 unless level_groups.keys.include? level }
+
+    level_groups.each do |key, value|
       active = project.sponsorship_levels.where("id = ? AND funding_goal > ?", key, value)
       sponsorship_levels_active << active if !active.size.zero?
     end
 
-    return sponsorship_levels_active.flatten
+    sponsorship_levels_active.flatten
   end
+
 end
