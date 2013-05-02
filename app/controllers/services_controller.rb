693ced6ce1e3a17ce3be89ac069d9a7cfe804f91
Dmitry Gritsay <tbizmy@gmail.com>
2013-05-02 03:56:11 +0300

    Add ability to link accounts that don't send user's email

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -21,7 +21,7 @@ class ServicesController < ApplicationController
         when "linkedin"
           uid = auth.uid
           email = auth.extra.raw_info.emailAddress
-          name = "auth.extra.raw_info.firstName auth.extra.raw_info.lastName"
+          name = "#{auth.extra.raw_info.firstName} #{auth.extra.raw_info.lastName}"
       end
 
       if uid.present? && provider.present?
@@ -42,67 +42,32 @@ class ServicesController < ApplicationController
           service = Service.find_by_provider_and_uid(provider, uid)
           if service
             flash[:notice] = "Successfully signed in via #{provider.capitalize}"
-            sign_in_and_redirect(:user, service.user)
+            sign_in_and_redirect(:user, user)
           else
-            # TODO: Register w/o email (Twitter)
-            if email.present?
-              user = User.find_by_email(email)
-              if user
-                user.services.create(:provider => provider, :uid => uid)
-                flash[:notice] = "Provider #{provider.capitalize} added to your account"
-                sign_in_and_redirect(:user, service.user)
-              else
-                user = User.new(:email => email, :name => name)
-                user.services.build(:provider => provider, :uid => uid)
-                user.save!
+            user = User.find_by_email(email)
+            if user
+              user.services.create(:provider => provider, :uid => uid)
+              flash[:notice] = "Provider #{provider.capitalize} added to your account"
+              sign_in_and_redirect(:user, user)
+            else
+              user = User.new(:email => email, :name => name)
+              user.services.build(:provider => provider, :uid => uid)
+              user.save
 
+              if user.persisted?
                 flash[:notice] = "Successfully registered via #{provider.capitalize}"
                 sign_in_and_redirect(:user, user)
+              else
+                session["devise.user_attributes"] = user.attributes
+                session["devise.provider"] = provider
+                session["devise.uid"] = uid
+                redirect_to new_user_registration_path
               end
             end
           end
         end
       end
     end
-
-    #auth = request.env['omniauth.auth']
-    ## Find an service here
-    #@service = Service.find_with_omniauth(auth)
-    #
-    #if @service.nil?
-    #  # If no service was found, create a brand new one here
-    #  @service = Service.create_with_omniauth(auth)
-    #end
-    #
-    #if signed_in?
-    #  if @service.user_id == current_user
-    #    # User is signed in so they are trying to link an service with their
-    #    # account. But we found the service and the user associated with it
-    #    # is the current user. So the service is already associated with
-    #    # this user. So let's display an error message.
-    #    logger.debug(1)
-    #    redirect_to root_url, notice: "Already linked that account!"
-    #  else
-    #    # The service is not associated with the current_user so lets
-    #    # associate the service
-    #    @service.user_id = current_user
-    #    @service.save()
-    #    logger.debug(2)
-    #    redirect_to root_url, notice: "Successfully linked that account!"
-    #  end
-    #else
-    #  if @service.user_id.present?
-    #    # The service we found had a user associated with it so let's
-    #    # just log them in here
-    #    self.current_user = @service.user_id
-    #    logger.debug(4)
-    #    redirect_to root_url, notice: "Signed in!"
-    #  else
-    #    # No user associated with the service so we need to create a new one
-    #    logger.debug(4)
-    #    redirect_to new_user_registration_url, notice: "Please finish registering"
-    #  end
-    #end
   end
 
   def destroy
