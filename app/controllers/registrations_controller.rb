ff11528ff873bf5b7caf057777485839dc7a8752
Dmitry Gritsay <tbizmy@gmail.com>
2013-05-02 16:00:04 +0300

    Disable asking current password when user changes profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,11 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
     email_changed = @user.email != params[:user][:email]
     password_changed = !params[:user][:password].empty?
 
-    successfully_updated = if email_changed or password_changed
-      @user.update_with_password(params[:user])
-    else
-      @user.update_without_password(params[:user])
-    end
+    successfully_updated = @user.update_attributes(params[:user])
 
     if successfully_updated
       # Sign in the user bypassing validation in case his password changed
@@ -16,7 +12,7 @@ class RegistrationsController < Devise::RegistrationsController
       if params[:user][:avatar]
         render json: { :path => (@user.errors.empty? ? @user.avatar_url : "#failed#") }
       else
-        redirect_to root_path
+        redirect_to @user
       end
     else
       render "edit"
