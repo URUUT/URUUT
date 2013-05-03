0e3307c2ce6ea555269b58c739046840523c30d0
hendra <hendra@41studio.com>
2013-05-03 17:09:23 +0700

    displayed sponsor in project details

--- app/controllers/project_admin/projects_controller.rb
+++ app/controllers/project_admin/projects_controller.rb
@@ -6,11 +6,12 @@ class ProjectAdmin::ProjectsController < ApplicationController
 	def index
 		@projects = Project.find(params[:id])
   end
-  
+
   def show
     @project = Project.find(params[:id])
     @donations = Donation.find_all_by_project_id(@project.id)
-    sponsors = ProjectSponsor.find_by_project_id(@project.id)
+    # sponsors = ProjectSponsor.find_by_project_id(@project.id)
+    sponsors = ProjectSponsor.where(@project.id)
     @sponsor_count = sponsors.nil? ? 0 : sponsors.count
     logger.debug(@sponsor_count)
     logger.debug(@project)
