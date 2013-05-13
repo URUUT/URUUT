87ddd165b7984d65304d0e3abe9a3bf7758667c3
Hendra Nicholas <hendra@41studio.com>
2013-05-13 20:04:01 +0700

    Fixed commit 76c236a

--- app/controllers/project_admin/projects_controller.rb
+++ app/controllers/project_admin/projects_controller.rb
@@ -1,6 +1,8 @@
 class ProjectAdmin::ProjectsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :set_project, except: [:index, :update, :show]
 
+  respond_to :js, except: [:index, :update, :show]
   layout false, :only => "stripe_update"
 
 	def index
@@ -12,9 +14,31 @@ class ProjectAdmin::ProjectsController < ApplicationController
     @donations = Donation.find_all_by_project_id(@project.id)
     sponsors = ProjectSponsor.find_by_project_id(@project.id)
     @sponsor_count = sponsors.nil? ? 0 : sponsors.count
+<<<<<<< HEAD
     logger.debug(@sponsor_count)
     logger.debug(@project)
     logger.debug(@donations)
   end
 
 end
+=======
+  end
+
+  def update
+    asd
+  end
+
+  def message; end
+
+  def overview; end
+
+  def manage; end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+end
+>>>>>>> 76c236a... add tab page function
