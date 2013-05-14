4afe9905e08b11aa0bac1d0a5ed565809efbdc06
Hendra Nicholas <hendra@41studio.com>
2013-05-14 19:36:07 +0700

    add page messages

--- app/controllers/project_admin/projects_controller.rb
+++ app/controllers/project_admin/projects_controller.rb
@@ -1,12 +1,12 @@
 class ProjectAdmin::ProjectsController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :set_project, except: [:index, :update, :show]
+  before_filter :set_project_ajax, except: [:index, :update, :show, :add_contact, :send_email]
 
   respond_to :js, except: [:index, :update, :show]
   layout false, :only => "stripe_update"
 
-	def index
-		@projects = Project.find(params[:id])
+  def index
+    @projects = Project.find(params[:id])
   end
 
   def show
@@ -21,16 +21,51 @@ class ProjectAdmin::ProjectsController < ApplicationController
     asd
   end
 
-  def message; end
+  def messages; end
 
   def overview; end
 
   def manage; end
 
+  def detail; end
+
+  def visual; end
+
+  def cover_photo_and_gallery; end
+
+  def post_to_social_media; end
+
+  def send_email
+    @error_messages = []
+
+    @error_messages << "email can't be blank" if params[:email_recepient].blank?
+    @error_messages << "email header can't be blank" if params[:email_header].blank?
+    @error_messages << "email content can't be blank" if params[:post]["email_content"].blank?
+
+    if @error_messages.blank?
+      ProjectMailer.project_message(params[:email_recepient], params[:email_header], params[:post]["email_content"]).deliver
+    end
+  end
+
+  def emails_page
+    @contact = Contact.new
+  end
+
+  def add_contact
+    @email_exists = true
+
+    if current_user.contacts.where(email: params[:email]).empty?
+      @email_exists = false
+      current_user.contacts.create(email: params[:email])
+    end
+  end
+
   private
 
-  def set_project
+  def set_project_ajax
     @project = Project.find(params[:project_id])
+    session[:current_project] = @project.id
   end
+
 
 end
