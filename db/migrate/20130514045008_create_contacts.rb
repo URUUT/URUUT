5e786175c8f0facb9a6bdd02fa51117860639c9d
Hendra Nicholas <hendra@41studio.com>
2013-05-14 19:33:58 +0700

    add model contact

---
+++ db/migrate/20130514045008_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :email
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
