c1da49a630870e3b20a4a6a2a0de31dd2b51b2fa
Hendra Nicholas <hendra@41studio.com>
2013-05-15 10:44:02 +0700

    Fixed conflict for cherry-pick 0103a79

--- app/controllers/project_admin/projects_controller.rb
+++ app/controllers/project_admin/projects_controller.rb
@@ -1,7 +1,5 @@
 class ProjectAdmin::ProjectsController < ApplicationController
   before_filter :authenticate_user!
-
-  before_filter :set_project_ajax, except: [:index, :update, :show, :add_contact, :send_email]
   respond_to :js, except: [:index, :update, :show, :emails_page]
   layout false, :only => "stripe_update"
 
@@ -11,10 +9,6 @@ class ProjectAdmin::ProjectsController < ApplicationController
   
   def show
     @project = Project.find(params[:id])
-    @donations = Donation.find_all_by_project_id(@project.id)
-    sponsors = ProjectSponsor.find_by_project_id(@project.id)
-    @sponsor_count = sponsors.nil? ? 0 : sponsors.count
-
     subheader
   end
 
@@ -50,6 +44,9 @@ class ProjectAdmin::ProjectsController < ApplicationController
 
   def emails_page
     @contact = Contact.new
+    @need_doctype = true
+
+    subheader
   end
 
   def add_contact
@@ -66,6 +63,13 @@ class ProjectAdmin::ProjectsController < ApplicationController
   def set_project_ajax
     @project = Project.find(params[:project_id])
     session[:current_project] = @project.id
+  end
+
+  def subheader
+    @donations = Donation.find_all_by_project_id(@project.id)
+    # sponsors = ProjectSponsor.find_by_project_id(@project.id)
+    sponsors = ProjectSponsor.where(@project.id)
+    @sponsor_count = sponsors.nil? ? 0 : sponsors.count
   end
 
 end
