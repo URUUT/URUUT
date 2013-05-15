d1b072dc1ec9c7af00330841c9cde4a06151cf2c
Hendra Nicholas <hendra@41studio.com>
2013-05-15 15:45:03 +0700

    change method for add contact on messages form

--- config/routes.rb
+++ config/routes.rb
@@ -54,8 +54,8 @@ Crowdfund::Application.routes.draw do
       get "emails_page"
 
       collection do
+        get "email_based_on_sponsor_level"
         post "send_email"
-        post "add_contact"
       end
     end
   end
