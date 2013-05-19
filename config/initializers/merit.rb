a2b1eb42d328b864b18827050b574be58a32d5f7
Dmitry Gritsay <tbizmy@gmail.com>
2013-05-19 05:13:45 +0300

    Define badges

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -4,15 +4,26 @@ Merit.setup do |config|
   # config.checks_on_each_request = true
 
   # Define ORM. Could be :active_record (default) and :mongo_mapper and :mongoid
-  # config.orm = :active_record
+  config.orm = :active_record
 
   # Define :user_model_name. This model will be used to grand badge if no :to option is given. Default is "User".
-  # config.user_model_name = "User"
+  config.user_model_name = "User"
 
   # Define :current_user_method. Similar to previous option. It will be used to retrieve :user_model_name object if no :to option is given. Default is "current_#{user_model_name.downcase}".
-  # config.current_user_method = "current_user"
+  config.current_user_method = "current_user"
 end
 
+Merit::Badge.create!({
+  :id => 1,
+  :name => "Project creation",
+  :description => "d"
+})
+
+Merit::Badge.create!({
+  :id => 2,
+  :name => "Project donation",
+  :description => "dd"
+})
 # Create application badges (uses https://github.com/norman/ambry)
 # Merit::Badge.create!({
 #   id: 1,
