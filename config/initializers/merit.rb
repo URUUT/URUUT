a4a71ec2dd74c3c6e602bb403e9c1d8ba55f07c0
Dmitry Gritsay <tbizmy@gmail.com>
2013-05-20 00:29:44 +0300

    Finish badges using placehold.it

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -16,18 +16,18 @@ end
 Merit::Badge.create!({
   :id => 1,
   :name => "Sign-up badge",
-  :custom_fields => { :image => "http://placehold.it/100x100" }
+  :custom_fields => { :image => "http://placehold.it/75x75" }
 })
 Merit::Badge.create!({
   :id => 2,
   :name => "Project creation badge",
-  :custom_fields => { :image => "http://placehold.it/100x100" }
+  :custom_fields => { :image => "http://placehold.it/75x75" }
 })
 
 Merit::Badge.create!({
   :id => 3,
   :name => "Project donation badge",
-  :custom_fields => { :image => "http://placehold.it/100x100" }
+  :custom_fields => { :image => "http://placehold.it/75x75" }
 })
 
 # Create application badges (uses https://github.com/norman/ambry)
