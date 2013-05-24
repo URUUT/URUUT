41baef5c1f6894aa43f9660dc1844dbe53a093e2
Chad Bartels <chad.bartels@gmail.com>
2013-05-24 13:34:31 -0400

    Replaced the sponsor payment form...again

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
 
   validates_presence_of :first_name
-   validates_presence_of :last_name
+  validates_presence_of :last_name
   # validate :minimum_image_size
   # validates_uniqueness_of :name, :email, :case_sensitive => false
 
