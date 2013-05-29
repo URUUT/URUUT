6b42576b4f860e6215635715696ae27eef63d444
Hendra Nicholas <hendra@41studio.com>
2013-05-29 18:24:55 +0700

    add mount uploader

--- app/models/project_sponsor.rb
+++ app/models/project_sponsor.rb
@@ -1,5 +1,5 @@
 class ProjectSponsor < ActiveRecord::Base
-  # mount_uploader :logo, LogoUploader
+  mount_uploader :logo, LogoUploader
 
   attr_accessible :card_token, :cost, :logo, :mission, :name, :project_id, :sponsor_id, :level_id, :payment, :status, :card_type, :card_last4
 
