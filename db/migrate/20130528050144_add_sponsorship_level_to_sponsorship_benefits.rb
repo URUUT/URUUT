12ad10eadaec0d29aeeaca8af32ff77fbbd260f9
Chad Bartels <chad.bartels@gmail.com>
2013-05-29 10:12:48 -0400

    Revert to 7dc26413

--- db/migrate/20130528050144_add_sponsorship_level_to_sponsorship_benefits.rb
+++
@@ -1,5 +0,0 @@
-class AddSponsorshipLevelToSponsorshipBenefits < ActiveRecord::Migration
-  def change
-    add_column :sponsorship_benefits, :sponsorship_level, :string
-  end
-end
--- db/migrate/20130528052919_add_cost_to_sponsorship_benefits.rb
+++
@@ -1,5 +0,0 @@
-class AddCostToSponsorshipBenefits < ActiveRecord::Migration
-  def change
-    add_column :sponsorship_benefits, :cost, :integer
-  end
-end
