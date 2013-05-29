d122df83bd021654a8f622f143f5055dc8eb5a7b
Chad Bartels <chad.bartels@gmail.com>
2013-05-29 15:15:47 -0400

    Brough close to current, still error with saving sponsorship benefits and levels

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,17 +44,9 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-<<<<<<< HEAD
     sort_sponsorships = @project.project_sponsors.sort_by {|ps| ps.level_id}
     @project_sponsors = sort_sponsorships.group_by {|sponsor| sponsor.level_id}
     @sponsorship_levels = SponsorshipLevel.all
-=======
-    @sponsorships = @project.sponsorship_benefits.where("project_id = ? AND status = ?", @project.id, "t")
-    @sponsorships.each do |s|
-      logger.debug(s.name)
-      logger.debug(SponsorshipBenefit.find(s.sponsorship_level_id))
-    end
->>>>>>> parent of 7023562... Revert to 7dc26413
     @donation = Donation.where("project_id = ?", @project.id)
     @perks = Perk.where("project_id = ?", @project.id)
     @user = User.find(@project.user_id)
@@ -64,17 +56,41 @@ class ProjectsController < ApplicationController
 
   def update
     @project = Project.find(params[:id])
-    sb_cost = @project.goal.to_i * 0.70
-    sb_platinum = params[:sponsorship_benefits]
-    SponsorshipBenefit.where(:project_id => @project.id).destroy_all
-    sb_platinum.each do |k, v|
-      v.each do |value|
-        @sponsors = SponsorshipBenefit.new(:project_id => @project.id, :sponsorship_level => k, :name => "#{value}", :cost => sb_cost)
-        @sponsors.save!
+    sponsorship_benefits = []
+
+    def sponsorship_benefit_level(level, benefit, key)
+      data = []
+      1.upto(params["#{level}_count"].to_i) do |x|
+        count = if !benefit[x - 1].blank?
+          !benefit[x - 1][:id].blank? ? benefit[x - 1][:id] : x
+        else
+          x
+        end
+        status = params["#{level}"]["#{count}"] ? 1 : 0
+        if @project.sponsorship_benefits.blank? || params["#{level}"]["id_#{count}"].nil?
+          unless params["#{level}"]["info_#{count}"].blank?
+            data <<  {name: params["#{level}"]["info_#{count}"],sponsorship_level_id: key, project_id: @project.id, status: status}
+          end
+        else
+          sponsorship_benefit = SponsorshipBenefit.find(params[level]["id_#{count}"])
+          sponsorship_benefit.update_attributes({name: params["#{level}"]["info_#{count}"],sponsorship_level_id: key, project_id: @project.id, status: status})
+        end
       end
+      data
     end
-    
-    # @sponsorship_benefits = SponsorshipBenefit.create(sb_platinum)
+
+    SponsorshipBenefit::SPONSORSHIP_BENEFITS.each do |key, value|
+
+      case key
+      when 1 then sponsorship_benefits += sponsorship_benefit_level("platinum", value, key)
+      when 2 then sponsorship_benefits += sponsorship_benefit_level("gold", value, key)
+      when 3 then sponsorship_benefits += sponsorship_benefit_level("silver", value, key)
+      when 4 then sponsorship_benefits += sponsorship_benefit_level("bronze", value, key)
+      end
+
+    end
+
+    @sponsorship_benefits = SponsorshipBenefit.create(sponsorship_benefits)
     @project.update_attributes!(params[:project])
 
     if @project.bitly.blank?
@@ -82,11 +98,9 @@ class ProjectsController < ApplicationController
       page_url = bitly.shorten("#{request.scheme}://#{request.host_with_port}/projects/#{@project.id}")
       @project.bitly = page_url.short_url
     end
-
-
     if @project.save
       respond_to do |format|
-        format.js { render :js => @project.id }
+        format.json { render :json => @project.id }
       end
     end
   end
