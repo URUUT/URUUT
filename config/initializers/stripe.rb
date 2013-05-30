c318eb8457cc9d46bc6c47687e44804416a24fad
Hendra Nicholas <hendra@41studio.com>
2013-05-30 14:56:14 +0700

    update stripe key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,9 @@
-API_KEY = "pk_test_BTNPlS59UIPJC5Fnp7CR9It6'"
-STRIPE_PUBLIC_KEY = "sk_test_Ydpgzl4aX7paVEPDaChNSP5Z"
+API_KEY = ""
+STRIPE_PUBLIC_KEY = ""
 
 Rails.configuration.stripe = {
   :publishable_key => STRIPE_PUBLIC_KEY,
   :secret_key      => API_KEY
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = ""
