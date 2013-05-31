686a8617f2359d05ce7cb87a25edd8c25d44fe95
Chad Bartels <chad.bartels@gmail.com>
2013-05-30 22:19:58 -0400

    fixing more for hendra pull request

--- app/models/sponsorship_benefit.rb
+++ app/models/sponsorship_benefit.rb
@@ -1,5 +1,5 @@
 class SponsorshipBenefit < ActiveRecord::Base
-  attr_accessible :name, :sponsorship_level, :project_id, :cost
+  attr_accessible :name, :sponsorship_level_id, :project_id, :status, :cost
 
   belongs_to :project
 
