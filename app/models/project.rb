2fbaa5b56a3bdce705ec21b42fffe6f78c28942d
Hendra Nicholas <hendra@41studio.com>
2013-05-31 11:09:17 +0700

    modify perk wizard

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
     :perks_attributes, :galleries_attributes, :status, :organization, :website, :twitter_handle, :facebook_page, :seed_video,
     :story, :about, :large_image, :seed_image, :cultivation_image, :ready_for_approval, :organization_type, :organization_classification, :cultivation_video
 
-  attr_accessor :sponsorship_permission, :perk_permission
+  attr_accessor :sponsorship_permission
   #validates :title, :short_description, :description, :presence => true, :if => :active?
   #validates :image, :address, :city, :state, :zip, :neighborhood, :duration, :goal, :presence => true, :if => :active_or_step1?
 
