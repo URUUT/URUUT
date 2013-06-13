8ce7ed8a0c8eea9f582e11d03234ea52b7a4b604
bayu41 <bayu@41studio.com>
2013-06-13 09:37:34 +0700

    fixed host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Crowdfund::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: "www.uruut.41studio.com" }
+  config.action_mailer.default_url_options = { host: "uruut.41studio.com" }
 
 
   config.action_mailer.smtp_settings = {
