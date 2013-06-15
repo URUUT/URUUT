6a97dffff58728962fd587a7c78b063718661838
Agung Yuliaji <agung.yuliaji@gmail.com>
2013-06-15 13:13:31 +0700

    add validation on registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,10 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name
   validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
+
   # validate :minimum_image_size
   validates_uniqueness_of :email, :case_sensitive => false
 
