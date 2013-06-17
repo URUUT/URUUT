baa244480f9c5932f1b94cd8e744651e78d6f5f0
bayu41 <bayu@41studio.com>
2013-06-17 20:23:11 +0700

    fixed redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,21 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
-    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
-    if request.referer == sign_in_url
-      project = Project.new
-      project.user_id = resource.id
-      project.save
-      edit_project_path(project.id)
-    else
-      stored_location_for(resource) || request.referer || root_path
+    begin
+      sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+      if request.referer == sign_in_url
+        project = Project.new
+        project.user_id = resource.id
+        project.save
+        edit_project_path(project.id)
+      else
+        stored_location_for(resource) || request.referer || root_path ||  request.env['omniauth.origin']
+      end
+      # logger.debug(request.referrer)
+      # return root_url
+    rescue
+      super
     end
-    # logger.debug(request.referrer)
-    # return root_url
   end
 
   protected
