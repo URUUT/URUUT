6a7f2c694113ad6412c108f34b23fd6adf5cc191
Agung Yuliaji <agung.yuliaji@gmail.com>
2013-06-17 15:06:53 +0700

    change cost to float, mod dependent destroy on project, and fix css

--- app/models/project.rb
+++ app/models/project.rb
@@ -16,11 +16,10 @@ class Project < ActiveRecord::Base
     project.has_many :perks
     project.has_many :galleries
     project.has_many :project_updates
+    project.has_many :project_sponsors
+    project.has_many :sponsorship_levels
+    project.has_many :sponsorship_benefits
   end
-
-  has_many :project_sponsors
-  has_many :sponsorship_levels
-  has_many :sponsorship_benefits
 
   accepts_nested_attributes_for :perks, allow_destroy: true
   accepts_nested_attributes_for :galleries, allow_destroy: true
