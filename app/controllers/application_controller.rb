6b7575a78d1582cda87cfc9d734f178c32b4de02
bayu41 <bayu@41studio.com>
2013-06-17 20:17:55 +0700

    fixed redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
     if request.referer == sign_in_url
-      super
+      project = Project.new
+      project.user_id = resource.id
+      project.save
+      edit_project_path(project.id)
     else
       stored_location_for(resource) || request.referer || root_path
     end
