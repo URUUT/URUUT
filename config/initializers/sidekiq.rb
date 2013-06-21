c9cdf75b38bc5faab92ddc0c20a528d08a9f3137
Chad Bartels <chad.bartels@gmail.com>
2013-06-20 22:06:39 -0400

    Trying to connect to redistogo on heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,6 @@
 # config/initializers/sidekiq.rb
 if Rails.env.production? || Rails.env.staging?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] }
-  end
-
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] }
+    config.redis = { :size => 1}
   end
 end
