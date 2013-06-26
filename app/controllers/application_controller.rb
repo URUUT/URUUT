a5bed6ce2b4bbf5b618c290a94f6dfebc0d54ee2
bayu41 <bayu@41studio.com>
2013-06-26 22:11:16 +0700

    testing purpose

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,25 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     begin
       sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+      puts sign_in_url
+      puts request.referer
       if session[:path] == "project_new"
         project = Project.new
         project.user_id = resource.id
         project.save
         session[:path] == ""
         edit_project_path(project.id)
+        puts "1"
       elsif request.referer == sign_in_url
         super
+        puts "2"
       else
         stored_location_for(resource) || request.referer || root_path ||  request.env['omniauth.origin']
+        puts "3"
       end
       # return root_url
     rescue
+      puts "4"
       super
     end
     # sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
