40c02d3554c949f99cb8cfb1e7e4492eb5fbabed
Chad Bartels <chad.bartels@gmail.com>
2013-07-02 23:15:03 -0400

    Adding more emails

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -24,4 +24,12 @@ class Sponsor < ActiveRecord::Base
   def payment_type_credit_card?
     self.payment_type.eql? "Credit Card"
   end
+  
+  def self.send_confirmation_email(sponsor)
+    SponsorMailer.new_sponsor(sponsor).deliver
+  end
+  
+  def self.sponsor_thank_you(sponsor)
+    SponsorMailer.sponsor_thank_you(sponsor).deliver
+  end
 end
