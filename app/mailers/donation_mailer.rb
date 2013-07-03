90395f569bb9c74491205e7b641c1a8cb545d938
Chad Bartels <chad.bartels@gmail.com>
2013-07-02 20:50:05 -0400

    Working on donation mailers

---
+++ app/mailers/donation_mailer.rb
@@ -0,0 +1,25 @@
+class DonationMailer < ActionMailer::Base
+  default from: "info@uruut.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.contact_confirmation.subject
+  #
+  def donation_confirmation(donation)
+    user = User.find_by_id(donation.user_id)
+    project = Project.find_by_id(donation.project_id)
+    sponsor = User.find_by_id(project.user_id)
+    
+    @donator_name = "#{user.first_name}" + " #{user.last_name}"
+    @email = user.email
+    @image = project.large_image
+    @host = ActionMailer::Base.default_url_options[:host]
+    @project = project
+    @project_title = project.title
+    @sponsor_email = sponsor.email
+    @sponsor_name = "#{sponsor.first_name}" + " #{sponsor.last_name}"
+
+    mail to: @email, subject: "Donor Thank You"
+  end
+end
