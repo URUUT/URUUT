60dfb0212d6e708779283ecc68624942857a01f2
Hendra Nicholas <hendra@41studio.com>
2013-07-22 10:15:40 +0700

    remove log

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_social_oauth(request.env["omniauth.auth"], current_user, "facebook")
-    logger.debug(request.env["omniauth.auth"])
     if @user.persisted?
       # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       sign_in(:user, @user)
@@ -37,7 +36,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, :kind => "LinkedIn") if is_navigational_format?
     else
       session["devise.linkedin_data"] = request.env["omniauth.auth"]
-      logger.debug(request.env["omniauth.auth"])
       # redirect_to new_user_registration_url
     end
   end
