93e802ba85dc21b3926a8a8d3f4853df352d8e7a
Chad Bartels <chad.bartels@gmail.com>
2013-09-23 02:26:20 -0400

    Update query for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       @projects_funded = @user.projects_funded.where("campaign_deadline #{comparison} ? AND live = 1", Time.now).order("updated_at DESC").page(params[:funded_page]).per(2)
       @pending_projects = @user.projects.where(live: 0)
     else
-      @projects_created = @user.projects.live.where("live = 1 AND status IS NULL").order("updated_at DESC").page(params[:created_page]).per(2)
+      @projects_created = @user.projects.live.order("updated_at DESC").page(params[:created_page]).per(2)
       @projects_funded = @user.projects_funded.live.order("updated_at DESC").page(params[:funded_page]).per(2)
       @pending_projects = @user.projects.where(live: 0)
     end
