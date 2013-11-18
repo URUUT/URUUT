90d93796d20e35bd798d73bbe39b52feb90e2049
Chad Bartels <chad.bartels@gmail.com>
2013-11-18 00:30:12 -0500

    updated sponsor benefits controller to extract method

--- app/models/sponsorship_benefit.rb
+++ app/models/sponsorship_benefit.rb
@@ -16,4 +16,34 @@ class SponsorshipBenefit < ActiveRecord::Base
                           {id: 2, desc: "Monthly plug on social media leading up to the project and the month after the project has been complete."},
                           {id: 3, desc: "Five tickets to VIP Gala Event."}],
                           4 => [{id: 1, desc: "Lorem ipsum dolor sit amet."}]}
+
+  def self.get_benefits_info(project, level_id)
+    case level_id
+      when 1
+        first_benefits = project.sponsorship_benefits.where(status: true).group_by {|sponsor| sponsor.sponsorship_level_id}[1]
+        cost = project.goal.to_i * 0.25
+        level = "Platinum"
+        session[:level_id] = 1
+      when 2
+        first_benefits = project.sponsorship_benefits.where(status: true).group_by {|sponsor| sponsor.sponsorship_level_id}[2]
+        cost = project.goal.to_i * 0.1
+        level = "Gold"
+        session[:level_id] = 2
+      when 3
+        first_benefits = project.sponsorship_benefits.where(status: true).group_by {|sponsor| sponsor.sponsorship_level_id}[3]
+        cost = project.goal.to_i * 0.05
+        level = "Silver"
+        session[:level_id] = 3
+      when 4
+        first_benefits = project.sponsorship_benefits.where(status: true).group_by {|sponsor| sponsor.sponsorship_level_id}[4]
+        if (project.goal.to_i * 0.02) >= 750
+          cost = 750
+        else
+          cost = project.goal.to_i * 0.02
+        end
+        level = "Bronze"
+        session[:level_id] = 4
+    end
+    return [first_benefits, cost, level, session[:level_id]]
+  end
 end
