dccbe6085c05b1db3b597b294ad83ba1d09c1ffc
Chad Bartels <chad.bartels@gmail.com>
2013-11-25 19:58:17 -0500

    Trying to get around constants issue

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,6 @@ ENV['RAILS_ENV'] ||= 'development'
 
 # Initialize the rails application
 Crowdfund::Application.initialize!
+
+AMOUNTS = [10, 25, 50, 100, 250]
+LEVELS = ["Level 1", "Level 2", "Level 3", "Level 4", "Level 5"]
