ed50a7777362f455c077dc7ef881a655ff40fc7e
Chad Bartels <chad.bartels@gmail.com>
2013-11-27 12:39:19 -0500

    Updated save video to remove http(s)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -217,7 +217,7 @@ class ProjectsController < ApplicationController
   end
 
   def save_video
-    video = params[:video_link].gsub(/^http(s)?/, '')
+    video = params[:video_link].gsub(/^(http|https)/, '')
     link_video = generate_video_link(video)
 
     @project = Project.find_by_id(session[:current_project])
