ff70225c09d62b137a2a2effb2024581569624cc
Chad Bartels <chad.bartels@gmail.com>
2013-12-04 11:12:50 -0500

    Update redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,4 @@
 require 'uri'
 
-if Rails.env == 'production'
-	uri = URI.parse(ENV["REDISTOGO_URL"])
-	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
