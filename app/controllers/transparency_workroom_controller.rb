e75f5609aab1330a57be71a4eb3402e625352587
Chad Bartels <chad.bartels@gmail.com>
2013-12-08 09:31:29 -0500

    Added TW index

--- app/controllers/transparency_workroom_controller.rb
+++ app/controllers/transparency_workroom_controller.rb
@@ -1,4 +1,12 @@
 class TransparencyWorkroomController < ApplicationController
+	layout "landing"
+
+	def index
+		@project = Project.find(params[:project_id])
+		@documents = @project.documents.all
+		@photo_vids = @project.galleries.media_transparent_room(params[:page])
+	end
+
 	def show
 	end
 end
