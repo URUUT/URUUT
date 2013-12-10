814a1feaa8b9132aa295de08357a1964543c1a7f
Ezequiel Delpero <edelpero@gmail.com>
2013-12-10 11:15:35 -0300

    [#61842430] Tests - User clicks on Get Started

--- spec/features/funding_new_project_spec.rb
+++ spec/features/funding_new_project_spec.rb
@@ -15,4 +15,17 @@ feature 'Funding a new project' do
     expect(page.body).to have_content('Get Started')
   end
 
+  scenario 'User clicks on Get Started', :js => true do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit new_project_path
+
+    within('.hero') do
+      click_button 'Get Started'
+    end
+
+    expect(page).to have_content('Basic Information')
+  end
+
 end
