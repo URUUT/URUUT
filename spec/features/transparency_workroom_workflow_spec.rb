6444ecb7ff3ca85566041f2a92c3b2683bf5a00a
Ezequiel Delpero <edelpero@gmail.com>
2013-12-17 13:26:48 -0300

    [#62256676] Tests - Updates User visits Project Admin Panel of the Project

--- spec/features/transparency_workroom_workflow_spec.rb
+++ spec/features/transparency_workroom_workflow_spec.rb
@@ -23,7 +23,7 @@ feature 'Transparency Woorkroom Workflow' do
     expect(page).to have_content('PROJECT ADMIN PANEL')
   end
 
-  scenario "User visits Project Admin Panel of the Project" do
+  scenario "User visits Project Admin Panel of the Project", js: true do
     @project.update_attributes(campaign_deadline: 3.days.from_now)
     visit project_admin_project_path(@project)
 
