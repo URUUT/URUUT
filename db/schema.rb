039c7bfaea44a2cb26994224489bdbc04313bfad
Chad Bartels <chad.bartels@gmail.com>
2013-12-24 01:18:14 -0500

    taking care of migration issue to prod

--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,11 @@ ActiveRecord::Schema.define(:version => 20131217011636) do
   add_index "badges_sashes", ["sash_id"], :name => "index_badges_sashes_on_sash_id"
 
   create_table "comments", :force => true do |t|
-    t.text    "body"
-    t.integer "post_id"
-    t.integer "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.text     "body"
+    t.integer  "post_id"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
@@ -188,8 +190,8 @@ ActiveRecord::Schema.define(:version => 20131217011636) do
     t.datetime "updated_at", :null => false
     t.string   "title"
     t.text     "body"
-    t.integer  "project_id"
     t.integer  "user_id"
+    t.integer  "project_id"
   end
 
   add_index "posts", ["project_id"], :name => "index_posts_on_project_id"
