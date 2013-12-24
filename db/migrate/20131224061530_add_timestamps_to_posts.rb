28376d37cc80ebd58eca437ece60514a85fc5a98
Chad Bartels <chad.bartels@gmail.com>
2013-12-24 01:21:52 -0500

    [62940386] Updated schema to add created/updated at fields to posts table

--- db/migrate/20131224061530_add_timestamps_to_posts.rb
+++ db/migrate/20131224061530_add_timestamps_to_posts.rb
@@ -1,9 +1,9 @@
 class AddTimestampsToPosts < ActiveRecord::Migration
   def change
-    unless column_exists? :created_at
+    unless column_exists? :posts, :created_at, :datetime
       add_column :posts, :created_at, :datetime
     end
-    unless column_exists? :updated_at
+    unless column_exists? :posts, :updated_at, :datetime
       add_column :posts, :updated_at, :datetime
     end
   end
