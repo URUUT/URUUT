1c3e310f4fad3cc7e319efe64d67488c3c0ca931
Chad Bartels <chad.bartels@gmail.com>
2013-12-24 01:21:52 -0500

    [62940386] Updated schema to add created/updated at fields to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131217011636) do
+ActiveRecord::Schema.define(:version => 20131224061530) do
 
   create_table "badges_sashes", :force => true do |t|
     t.integer  "badge_id"
