b602114f3ac8e95073f19d14e10ff0b412151dec
Sergio Sebastian Arriagada <sergio@hashdog.com>
2014-01-09 16:33:28 -0300

    [#62940480] Donor to ask a question on TW

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131224061530) do
+ActiveRecord::Schema.define(:version => 20140109180139) do
 
   create_table "badges_sashes", :force => true do |t|
     t.integer  "badge_id"
@@ -299,6 +299,15 @@ ActiveRecord::Schema.define(:version => 20131224061530) do
   end
 
   add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
+
+  create_table "questions", :force => true do |t|
+    t.string   "subject"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "project_id"
+  end
 
   create_table "sashes", :force => true do |t|
     t.datetime "created_at", :null => false
