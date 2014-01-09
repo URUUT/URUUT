8d8a76c466cd669b0292b8c1c353b4f7abdd4220
Sergio Sebastian Arriagada <sergio@hashdog.com>
2014-01-09 14:31:18 -0300

    [#62940480] Donor to ask a question on TW

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -4,7 +4,7 @@ class QuestionController < ApplicationController
 		if @question.save
 	      flash[:notice] = "Successfully send question."
 	      user_name = "#{current_user.first_name} #{current_user.last_name}"
-	      QuestionMailer.send_question(user_name, current_user.email, @question.subject, @question.body)
+	      QuestionMailer.send_question(user_name, current_user.email, @question.subject, @question.body).deliver
 	      redirect_to :back
 	    else
 	      flash[:error] = "Error question."
