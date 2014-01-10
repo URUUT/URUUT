ba185b79a13ddf7ebaab67e2420967adcd1d962f
Ezequiel Delpero <edelpero@gmail.com>
2014-01-10 15:37:37 -0300

    [#60735844] Adds active? and canceled? methods to Subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,13 @@
 class Subscription < ActiveRecord::Base
   belongs_to :membership
   attr_accessible :stripe_plan_id
+
+  def active?
+    canceled_at.blank?
+  end
+
+  def canceled?
+    !active?
+  end
+
 end
