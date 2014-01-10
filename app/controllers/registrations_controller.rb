b09e7caf53f38b4cdfd26741bf86c3838c0d37da
Ezequiel Delpero <edelpero@gmail.com>
2014-01-10 10:24:41 -0300

    [#60735812] Creates membership for user after he signs up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,25 @@
 class RegistrationsController < Devise::RegistrationsController
+
+  def create
+    build_resource
+
+    if resource.save
+      resource.build_membership.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   def update
     @user = User.find(current_user.id)
     email_changed = @user.email != params[:user][:email]
