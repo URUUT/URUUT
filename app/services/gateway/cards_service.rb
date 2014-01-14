263ad0a43c91a6372826f41f10e5aebcce9a72d9
Ezequiel Delpero <edelpero@gmail.com>
2014-01-14 14:58:33 -0300

    [#63748284] Updates Gateway::CardsService#create

--- app/services/gateway/cards_service.rb
+++ app/services/gateway/cards_service.rb
@@ -5,12 +5,15 @@ class Gateway::CardsService < Gateway::BaseService
   def create(number, exp_month, exp_year, cvc)
     return false unless find_customer
 
-    customer.cards.create(
+    response = customer.cards.create(
       number:    number,
       exp_month: exp_month,
       exp_year:  exp_year,
       cvc:       cvc
     )
+
+    user.stripe_card_token = response.id
+    user.save
   end
 
 end
