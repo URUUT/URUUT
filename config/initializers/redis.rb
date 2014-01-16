e92cebd75a791c07bb3ca4108e752dbb2203770a
Chad Bartels <chad.bartels@gmail.com>
2014-01-16 09:52:53 -0500

    Trying to alleviate Redis initializer error about invalid URI

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 if Rails.env == 'development' || Rails.env == 'production'
 	uri = URI.parse(ENV["REDISTOGO_URL"])
 	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
