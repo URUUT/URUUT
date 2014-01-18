836318b34cae63104a969d86f41bb9fa27e0c08e
Chad Bartels <chad.bartels@gmail.com>
2014-01-18 01:27:18 -0500

    Fixed validation on Tax Report

--- app/models/tax_report.rb
+++ app/models/tax_report.rb
@@ -1,7 +1,7 @@
 class TaxReport < ActiveRecord::Base
   attr_accessible :url, :user_id, :project_id
 
-  validates_uniqueness_of :url, :scope => [:project_id]
+  # validates_uniqueness_of :url, :scope => [:project_id]
 
   belongs_to :user
   belongs_to :project
