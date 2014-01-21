7e63bc6aaf13f66211d6504ba1f01c758211fd85
Ezequiel Delpero <edelpero@gmail.com>
2014-01-21 14:53:05 -0300

    [#60076400] Create TechBridge instance of URUUT - Updates deploy recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ namespace :db do
   end
 
   task :load_schema, :except => { :no_release => true }, :roles => :app do
-    run "cd #{current_path}; bundle exec rake db:schema:load RAILS_ENV=#{rails_env}"
+    run "cd #{release_path}; bundle exec rake db:schema:load RAILS_ENV=#{rails_env}"
   end
 end
 
@@ -39,31 +39,31 @@ namespace :deploy do
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
       #monkey path
-      run "#{sudo} /etc/init.d/unicorn_techbridge stop"
-      run "#{sudo} rm -f /u/apps/techbridge/shared/pids/unicorn.pid"
-      run "#{sudo} /etc/init.d/unicorn_techbridge start"
+      run "cd #{release_path};/etc/init.d/unicorn_techbridge stop"
+      run "rm -f /u/apps/techbridge/shared/pids/unicorn.pid"
+      run "cd #{release_path};/etc/init.d/unicorn_techbridge start"
     end
   end
 
   desc "reload the database with seed data"
   task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    run "cd #{release_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
   end
 
   desc "Clear Tmp folder"
   task :seed do
-    run "cd #{current_path}; bundle exec rake tmp:clear RAILS_ENV=#{rails_env}"
+    run "cd #{release_path}; bundle exec rake tmp:clear RAILS_ENV=#{rails_env}"
   end
 
 
   desc "Change File Permissions on Temp folder"
   task :change_file_permission do
-    run "cd #{current_path}; #{sudo} chmod -R 777 tmp/"
+    run "cd #{release_path}; #{sudo} chmod -R 777 tmp/"
   end
 
   desc "Install new gems if Gemfile's updated"
   task :bundle_install do
-    run "cd #{current_path}; bundle install"
+    run "cd #{release_path}; bundle install"
   end
 end
 
