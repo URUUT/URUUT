1eff35ba7ad7d7ca33cf8210236c73ed5578fe99
Ezequiel Delpero <edelpero@gmail.com>
2014-01-21 10:25:08 -0300

    [#60076400] Create TechBridge instance of URUUT - Updates Redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-if Rails.env == 'development' || Rails.env == 'production'
-	uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDISTOGO_URL"])
+if Rails.env == 'development' || Rails.env == 'staging'
 	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+elsif Rails.env.production?
+  REDIS = Redis.new(:host => uri.host, :port => uri.port)
 end
