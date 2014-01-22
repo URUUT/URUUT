c9d670c23e9e2a5354ea417e5bdc0637f2c5df9c
Ezequiel Delpero <edelpero@gmail.com>
2014-01-22 16:34:41 -0300

    [#64050246] Updates Gateway::CardsService to receive a CreditCard object

--- app/services/gateway/cards_service.rb
+++ app/services/gateway/cards_service.rb
@@ -2,17 +2,15 @@ require 'stripe'
 
 class Gateway::CardsService < Gateway::BaseService
 
-  def create(number, exp_month, exp_year, cvc)
+  def create(credit_card)
     return false unless find_customer
 
     response = customer.cards.create(
-      card: {
-        number:    number,
-        exp_month: exp_month,
-        exp_year:  exp_year,
-        cvc:       cvc
-      }
+      card: credit_card.details_as_hash
     )
+
+    customer.default_card = response.id
+    customer.save
 
     user.stripe_card_token = response.id
     user.save
