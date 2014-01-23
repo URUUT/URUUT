e616b2f8625504a7539bd5eefaac32c3a56fa250
Ezequiel Delpero <edelpero@gmail.com>
2014-01-23 14:57:53 -0300

    [#60724320] Adds plan_id to memberships' table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140123175012) do
+ActiveRecord::Schema.define(:version => 20140123175450) do
 
   create_table "accounts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -127,8 +127,10 @@ ActiveRecord::Schema.define(:version => 20140123175012) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "sponsor_id"
+    t.integer  "plan_id"
   end
 
+  add_index "memberships", ["plan_id"], :name => "index_memberships_on_plan_id"
   add_index "memberships", ["sponsor_id"], :name => "index_memberships_on_sponsor_id"
   add_index "memberships", ["user_id"], :name => "index_memberships_on_user_id"
 
