f39fe376ffa1cf432c8e1b525328cccc39575f14
Sergio Sebastian Arriagada <sergio@hashdog.com>
2014-01-23 12:07:28 -0300

    [#60724320] Create SaaS Plans for different Features

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   layout "application", :except => [:index, :discover, :about, :contact]
-  layout "landing", :only => [:index, :discover, :about, :home, :funding_sources, :search, :pricing_page]
+  layout "landing", :only => [:index, :discover, :about, :home, :funding_sources, :search, :pricing]
   skip_before_filter :set_previous_page
   before_filter :set_session_page
   before_filter :set_session_wizard, except: :home
