41b367ad84d1918b883544df88d24f97b80e0f9d
Ezequiel Delpero <edelpero@gmail.com>
2014-01-23 17:15:07 -0300

    [#60724320] User can update his plan

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,15 +1,15 @@
 class MembershipsController < ApplicationController
 
   before_filter :authenticate_user!
-  before_filter :find_membership, only: [:show, :edit]
+  before_filter :find_membership, only: [:show, :update, :edit]
 
-  def create
-    user_membership = current_user.membership
-    user_membership.kind = params[:membership_type]
+  def update
+    plan = Plan.where(name: params[:membership_type]).first
+    @membership.plan = plan
 
-    if user_membership.save
+    if @membership.save
       flash[:notice] = 'You have not been subscribed.'
-      redirect_to user_membership_path(current_user, user_membership)
+      redirect_to user_membership_path(current_user, @membership)
     else
       flash[:alert] = 'You have not been subscribed.'
       redirect_to pricing_page_pages_path()
