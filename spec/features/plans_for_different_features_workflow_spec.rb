962307b2bdf197aa25ebcb755a814b8a03d0098d
Ezequiel Delpero <edelpero@gmail.com>
2014-01-24 11:28:13 -0300

    [#64234010] Test - User choose a fee-based plan

--- spec/features/plans_for_different_features_workflow_spec.rb
+++ spec/features/plans_for_different_features_workflow_spec.rb
@@ -5,6 +5,10 @@ feature 'Change Plan Workflow' do
     @user = FactoryGirl.create(:user)
     @user.build_membership.save
     login_as(@user, :scope => :user)
+
+    ['fee', 'basic', 'plus'].each do |plan_name|
+      Plan.new(name: plan_name).save
+    end
   end
 
   scenario 'User visits his account information' do
@@ -22,6 +26,17 @@ feature 'Change Plan Workflow' do
     within('.our-story:first-of-type') do
       expect(page).to have_content("All The Features You Need at a Sensible Price.")
     end
+  end
+
+  scenario 'User choose a fee-based plan', js: true do
+    visit edit_user_registration_path
+    click_link 'Change your subscription plan.'
+
+    within('.plan:first-of-type') do
+      click_button 'Select ►'
+    end
+
+    expect(page).to have_content('Your membership is FEE')
   end
 
 end
