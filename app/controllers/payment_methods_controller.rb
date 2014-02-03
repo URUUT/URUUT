43fbfde11227a29a48863860107d420f482f5cee
Claudio Ceballos Paz <claudiocebpaz@gmail.com>
2014-02-03 14:11:39 -0300

    fixed task/bug 47

--- app/controllers/payment_methods_controller.rb
+++ app/controllers/payment_methods_controller.rb
@@ -17,7 +17,7 @@ class PaymentMethodsController < ApplicationController
 
     if @credit_card.valid? && @card_service.create(@credit_card)
       customer_plan.update_plan(plan_id) if plan_id.present?
-      redirect_to new_project_path
+      redirect_to users_sign_up_confirmation_path
     else
       render :new
     end
