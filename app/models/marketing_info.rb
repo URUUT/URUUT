10ba265d040a206e177a2abb07de87c64855a699
Claudio Ceballos Paz <claudiocebpaz@gmail.com>
2014-02-05 17:22:02 -0300

    [#65074362] Create modal for Marketing Sign Up form

--- app/models/marketing_info.rb
+++ app/models/marketing_info.rb
@@ -3,7 +3,7 @@ class MarketingInfo < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :email, :first_name, :last_name, presence: true
+  # validates :email, :first_name, :last_name, presence: true
   # validates_uniqueness_of :email, case_sensitive: false
 
 end
