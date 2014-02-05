220d66f81742c7a4f0c71531a1188177d2330c26
Claudio Ceballos Paz <claudiocebpaz@gmail.com>
2014-02-05 16:52:58 -0300

    WIP [#65074362] Create modal for Marketing Sign Up form

--- app/controllers/marketing_infos_controller.rb
+++ app/controllers/marketing_infos_controller.rb
@@ -3,9 +3,11 @@ class MarketingInfosController < ApplicationController
   def create
     @marketing_info = MarketingInfo.new(first_name: params[:marketing_info][:first_name], last_name: params[:marketing_info][:last_name], email: params[:marketing_info][:email])
     if @marketing_info.save
-      redirect_to params[:marketing_info][:href]
+      url = params[:marketing_info][:href]
+      url << "&marketing_info_id=#{@marketing_info.id}"
+      redirect_to url
     else
-      redirect_to choose_plan_pages_path
+      redirect_to choose_plan_pages_path, notice: 'Email error'
     end
   end
 
--- app/models/marketing_info.rb
+++ app/models/marketing_info.rb
@@ -4,6 +4,6 @@ class MarketingInfo < ActiveRecord::Base
   belongs_to :user
 
   validates :email, :first_name, :last_name, presence: true
-  validates_uniqueness_of :email, case_sensitive: false
+  # validates_uniqueness_of :email, case_sensitive: false
 
 end
