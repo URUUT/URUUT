a33a4453a9a6383dba6c058f9686c764cd34a19b
Ezequiel Delpero <edelpero@gmail.com>
2014-02-06 12:29:42 -0300

    Updates MarkingInfosController

--- app/controllers/marketing_infos_controller.rb
+++ app/controllers/marketing_infos_controller.rb
@@ -1,9 +1,9 @@
 class MarketingInfosController < ApplicationController
 
   def create
-    @marketing_info = MarketingInfo.new(first_name: params[:marketing_info][:first_name], last_name: params[:marketing_info][:last_name], email: params[:marketing_info][:email])
+    @marketing_info = MarketingInfo.new(params[:marketing_info])
     if @marketing_info.save
-      url = params[:marketing_info][:href]
+      url = @marketing_info.href
       url << "&marketing_info_id=#{@marketing_info.id}"
       redirect_to url
     else
