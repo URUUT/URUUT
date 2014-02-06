5713d674ef2d2bb4fb059c5dc8f8f75b02aac2b1
Ezequiel Delpero <edelpero@gmail.com>
2014-02-06 15:00:04 -0300

    [#65284510] Modify Membership section based on Plan selected

--- app/helpers/memberships_helper.rb
+++ app/helpers/memberships_helper.rb
@@ -11,11 +11,29 @@ module MembershipsHelper
   def membership_cost(user)
     case user.membership.kind
     when 'fee'
-      '5%'
+      '5% of any amount raised on each project'
     when 'basic'
-      '$99'
+      '$99 every 1 month'
     when 'plus'
-      '$149'
+      '$149 every 1 month'
+    end
+  end
+
+  def membership_cost_message_start(user)
+    case user.membership.kind
+    when 'fee'
+      'subscription means you will be charged'
+    when 'basic', 'plus'
+      "subscription will be automatically renewed #{next_membership_renewal_date(user)} and charged"
+    end
+  end
+
+  def membership_cost_message_end(user)
+    case user.membership.kind
+    when 'fee'
+      'unless you change or cancel it.'
+    when 'basic', 'plus'
+      'unless you cancel your subscription before that time.'
     end
   end
 
