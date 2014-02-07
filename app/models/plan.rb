4d2d895ad8e503c7c35c9ccf2f40cec8ae0ffdff
Ezequiel Delpero <edelpero@gmail.com>
2014-02-07 10:01:48 -0300

    Update tax_receipts task

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,7 +5,7 @@ class Plan < ActiveRecord::Base
 
   attr_accessible :stripe_plan_id, :name
 
-  def have_feature?(feature_name)
+  def has_feature?(feature_name)
     features.where(name: feature_name).exists?
   end
 end
