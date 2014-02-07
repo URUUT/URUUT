3a0d8fa27adfecd5eb49016ec8b8b1e0211e10a0
Claudio Ceballos Paz <claudiocebpaz@gmail.com>
2014-02-07 11:12:59 -0300

    WIP [#65074068] Add Whitepaper email

--- app/controllers/demo_request_controller.rb
+++ app/controllers/demo_request_controller.rb
@@ -50,6 +50,7 @@ class DemoRequestController < ApplicationController
   end
 
   def whitepaper
+    DemoRequestMailer.whitepaper_demo_confirmation(@demo).deliver
     send_file 'public/whitepaper.pdf'
   end
 end
