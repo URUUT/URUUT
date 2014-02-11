a2446482a40e94321553594da4896bb677869396
Ezequiel Delpero <edelpero@gmail.com>
2014-02-11 15:56:32 -0300

    [#65549068] Create Cancel membership method - Fixes when upgrading from basic or plus to fee

--- app/services/gateway/plans_service.rb
+++ app/services/gateway/plans_service.rb
@@ -3,11 +3,9 @@ require 'stripe'
 class Gateway::PlansService < Gateway::BaseService
 
   def update_plan(plan_id)
-    plan = Plan.where(name: plan_id).first
-    user_membership = @user.membership
-    user_membership.plan = plan
-
-    update_stripe_subscription(user_membership, plan_id)
+    membership = @user.membership
+    cancel_plan if updates_from_basic_or_plus_to_fee?(membership, plan_id)
+    update_stripe_subscription(membership, plan_id)
   end
 
   def cancel_plan
@@ -15,21 +13,25 @@ class Gateway::PlansService < Gateway::BaseService
     return false unless find_card && membership.stripe_subscription_id
 
     cancel_stripe_subscription(membership)
+    membership.plan = nil
     membership.stripe_subscription_id = nil
     membership.save
   end
 
 private
 
-  def update_stripe_subscription(user_membership, plan_id)
+  def update_stripe_subscription(membership, plan_id)
+    plan = Plan.where(name: plan_id).first
+    membership.plan = plan
+
     case plan_id
     when 'fee'
-      true
+      membership.save
     when 'basic', 'plus'
       find_card
       response = customer.update_subscription(plan: plan_id)
-      user_membership.stripe_subscription_id = response.id
-      user_membership.save
+      membership.stripe_subscription_id = response.id
+      membership.save
     else
       false
     end
@@ -40,5 +42,9 @@ private
     when 'basic', 'plus'
       customer.cancel_subscription
     end
+  end
+
+  def updates_from_basic_or_plus_to_fee?(membership, plan_id)
+    plan_id == 'fee' && ['basic', 'plus'].include?(membership.kind)
   end
 end
