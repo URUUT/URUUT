372f88c6f1653d7536c11422a19535d9948fcc50
amancke <alejo@bandofcoders.com>
2014-02-10 23:01:29 -0300

    Change DEMO mail subject

--- app/mailers/demo_request_mailer.rb
+++ app/mailers/demo_request_mailer.rb
@@ -5,6 +5,6 @@ class DemoRequestMailer < ActionMailer::Base
   def scheduled_demo_confirmation(demo_request)
     @user = demo_request
 
-    mail to: demo_request.email, bcc: "kbush@uruut.com, mfeinberg@uruut.com, bnorwood@uruut.com, alejo+bcc@bandofcoders.com", subject: 'Scheduled Your URUUT Demo'
+    mail to: demo_request.email, bcc: "kbush@uruut.com, mfeinberg@uruut.com, bnorwood@uruut.com, alejo+bcc@bandofcoders.com", subject: 'Scheduling Your URUUT Demo'
   end
 end
