f90b9873e6e1cf83e86aaed424939f169f3b3e03
Ezequiel Delpero <edelpero@gmail.com>
2014-02-20 15:19:59 -0300

    Updates assets config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,16 +9,16 @@ Crowdfund::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   config.static_cache_control = "public, max-age=#{1.year.to_i}"
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # config.assets.compress = true
   config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
