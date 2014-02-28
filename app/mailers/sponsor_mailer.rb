48980064e15d6993cff099803a8fbfc593ae201f
Claudio Ceballos Paz <sergio@hashdog.com>
2014-02-28 10:42:26 -0300

    [#66660742] Add new username/login to enter the Wizard

--- app/mailers/sponsor_mailer.rb
+++ app/mailers/sponsor_mailer.rb
@@ -4,7 +4,7 @@ class SponsorMailer < ActionMailer::Base
   helper MailerHelper
   include MailerHelper
 
-  default from: "info@techbridge.com"
+  default from: "xchange@techbridge.org"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
