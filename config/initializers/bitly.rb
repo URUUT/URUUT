15b1b5ffb941e1bd73cd2baf496fa5aa3b9ab515
Chad Bartels <chad.bartels@gmail.com>
2014-03-04 13:51:47 -0500

    Uncommented Bitly config

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -1,5 +1,5 @@
-# Bitly.configure do |config|
-#   config.api_version = 3
-#   config.login = "chadbartels"
-#   config.api_key = "R_b1c64c4fb7afc739d0e2da6bcdaf946b"
-# end
+Bitly.configure do |config|
+  config.api_version = 3
+  config.login = "chadbartels"
+  config.api_key = "R_b1c64c4fb7afc739d0e2da6bcdaf946b"
+end
