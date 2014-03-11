bca1921f970a69ea6256e96719766b7149cc3bfa
Claudio Ceballos Paz <sergio@hashdog.com>
2014-03-11 09:50:30 -0300

    [#67231562] Sign In not working (Needs refresh)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -28,7 +28,7 @@ class Users::SessionsController < DeviseController
     # else
     #   respond_to :js
     # end
-    respond_with resource, :location => after_sign_out_path_for(resource_name)
+    respond_with resource, :location => get_redirect_path
   end
 
   # DELETE /resource/sign_out
