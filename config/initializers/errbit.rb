b0dde876ea1f5aec8b296f1db8dd585994259e5d
Chad Bartels <chad.bartels@gmail.com>
2014-03-12 17:15:11 -0400

    Removed javascript error catching from errbit and added TB instance of app to Errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = '4881a9b2f302f8be1869975521664871'
+  config.api_key = '0b0c7afbf771f20aaf92ef9ca90bc90b'
   config.host    = 'uruut-errbit.herokuapp.com'
   config.port    = 80
   config.secure  = config.port == 443
