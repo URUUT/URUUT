9b46876dda90256e4b082f132a230221299e1f73
Ezequiel Delpero <edelpero@gmail.com>
2014-03-14 12:49:11 -0300

    [#67551316] Restores XLS generator

--- app/services/worksheet/donors.rb
+++ app/services/worksheet/donors.rb
@@ -1,8 +1,34 @@
 class Worksheet::Donors < Worksheet::Base
 
+  def initialize(xls, project)
+    @project = project
+    super(xls, project, funders, 'Donors')
+  end
+
 private
-  def attributes
-    [:email, :first_name, :last_name, :neighborhood, :city, :state, :zip,
-     :website, :organization, :level, :uruut_point]
+  attr_reader :project
+
+  def attributes(funder)
+    funder_donation = donation(funder)
+
+    { project_name: project.title,
+      email:        funder.email,
+      first_name:   funder.first_name,
+      last_name:    funder.last_name,
+      amount:       funder_donation.amount,
+      perk:         funder_donation.perk_name,
+      description:  funder_donation.description }
+  end
+
+  def funders
+    @funders = User.unique_project_donors(project)
+  end
+
+  def donation(funder)
+    Donation.with_funder(funder).with_project(project).first
+  end
+
+  def column_titles
+    [:project_name, :email, :first_name, :last_name, :amount, :perk, :description]
   end
 end
