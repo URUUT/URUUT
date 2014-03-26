75c6bad2e0a11c2c635bf297274fd1b3ced2f8c6
Chad Bartels <chad.bartels@gmail.com>
2014-03-26 15:27:41 -0400

    Updated visiting home page feature

--- spec/features/visiting_the_root_page_spec.rb
+++ spec/features/visiting_the_root_page_spec.rb
@@ -4,7 +4,7 @@ feature 'Visiting the root page' do
 
   scenario 'Visit home page without being signed in' do
     visit root_url
-    expect(page.body).to have_content('Log In')
+    expect(page.body).to have_link('Log In')
   end
 
 end
