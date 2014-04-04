07f6fafe0b547c32fcbb7f1d0ed03af3a30c2606
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-04 11:21:00 -0300

    Changes default confirm modal to custom bootstrap modal. [#68321392]

--- app/assets/javascripts/customSponsorshipBehavior.js
+++ app/assets/javascripts/customSponsorshipBehavior.js
@@ -1,14 +1,12 @@
 $('#next-link-to-assets').unbind();
-$('#next-link-to-assets').click( function (e) {
+$('#confirm-changes .btn-primary').click( function (e) {
   e.preventDefault();
-  if ( confirm('Are you sure? Your changes can not be changed.') ) {
-    $.ajax({
-      url: custom_level_url_path,
-      type: 'PUT',
-      data: $('[name^=level]')
-    }).done(function(data) {
-      window.location.href = '/projects/' + projectId + '/edit#assets';
-      window.location.reload(true);
-    });
-  }
+  $.ajax({
+    url: custom_level_url_path,
+    type: 'PUT',
+    data: $('[name^=level]')
+  }).done(function(data) {
+    window.location.href = '/projects/' + projectId + '/edit#assets';
+    window.location.reload(true);
+  });
 });
