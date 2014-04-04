f2c139148fba1b39d0fd30f512f0794fcedbbd34
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-04 16:22:16 -0300

    Changes integration tests. [#68321456]

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -116,8 +116,9 @@ module ProjectsHelper
     project.project_sponsors.any?
   end
 
-  def level_name(index, level, default)
-    (@sponsorship_levels[index])? @sponsorship_levels[index].name : default
+  def level_name(index, default, levels)
+    return default unless levels.any?
+    (levels[index])? levels[index].name : default
   end
 
 end
