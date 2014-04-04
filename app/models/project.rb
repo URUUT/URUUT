4bb8cfeb3bd36eab5141816d8a3ea722050f0a8d
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-04 12:25:16 -0300

    Adds check if a project is live in the modals. [#68321392]

--- app/models/project.rb
+++ app/models/project.rb
@@ -442,6 +442,9 @@ class Project < ActiveRecord::Base
     documents_updated_since(date).any? ||
     posts_updated_since(date).any?     ||
     galleries_updated_since(date).any?
+
+  def live?
+    live == 1
   end
 
 private
