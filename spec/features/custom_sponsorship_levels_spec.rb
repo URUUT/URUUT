c622d9b558fa5afa053a45c8eaf39fa82237ea86
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-07 08:52:52 -0300

    Adds views spec. [#68321456]

--- spec/features/custom_sponsorship_levels_spec.rb
+++ spec/features/custom_sponsorship_levels_spec.rb
@@ -13,22 +13,6 @@ feature 'Custom sponsorship creation' do
     login_as(@user, :scope => :user)
   end
 
-  scenario "Should display level name fields", js: true do
-    @project.live = 0
-    @project.save!
-    
-    page.set_rack_session(connected: true)
-
-    visit edit_project_path(@project, anchor: 'sponsorship')
-
-    select 'Yes', from: 'project_sponsorship_permission'
-
-    expect( page.has_field?('level[platinum][name]') ).to be_true
-    expect( page.has_field?('level[gold][name]') ).to be_true
-    expect( page.has_field?('level[silver][name]') ).to be_true
-    expect( page.has_field?('level[bronze][name]') ).to be_true
-  end
-#
 #  scenario "Should create custom level name", js: true do
 #    @project.live = 0
 #    @project.save!
@@ -58,18 +42,35 @@ feature 'Custom sponsorship creation' do
 #    #expect(@project.sponsorship_benefits.where(sponsorship_level_id: 2)).to exist
 #  end
 #
-#  scenario "A live project can't edit custom level names", js: true do
-#    page.set_rack_session(connected: true)
-#
-#    visit edit_project_path(@project, anchor: 'sponsorship')
-#
-#    select 'Yes', from: 'project_sponsorship_permission'
-#
-#    expect( page.has_field?('level[platinum][name]') ).to be_false
-#    expect( page.has_field?('level[gold][name]') ).to be_false
-#    expect( page.has_field?('level[silver][name]') ).to be_false
-#    expect( page.has_field?('level[bronze][name]') ).to be_false
-#  end
+
+  scenario "Should display level name fields", js: true do
+    @project.live = 0
+    @project.save!
+    
+    page.set_rack_session(connected: true)
+
+    visit edit_project_path(@project, anchor: 'sponsorship')
+
+    select 'Yes', from: 'project_sponsorship_permission'
+
+    expect( page.has_field?('level[platinum][name]') ).to be_true
+    expect( page.has_field?('level[gold][name]') ).to be_true
+    expect( page.has_field?('level[silver][name]') ).to be_true
+    expect( page.has_field?('level[bronze][name]') ).to be_true
+  end
+
+  scenario "A live project can't edit custom level names", js: true do
+    page.set_rack_session(connected: true)
+
+    visit edit_project_path(@project, anchor: 'sponsorship')
+
+    select 'Yes', from: 'project_sponsorship_permission'
+
+    expect( page.has_field?('level[platinum][name]') ).to be_false
+    expect( page.has_field?('level[gold][name]') ).to be_false
+    expect( page.has_field?('level[silver][name]') ).to be_false
+    expect( page.has_field?('level[bronze][name]') ).to be_false
+  end
 #
 #  scenario "Should not display level name fields if there is not a Plus User", js: true do
 #    @plan.name = 'basic'
