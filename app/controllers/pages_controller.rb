5353f55fb3388d11f982b7aacf7f905e5079bf71
Chad Bartels <chad.bartels@gmail.com>
2014-04-10 22:17:26 -0400

    Updated pagination on Fund Projects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,8 +10,8 @@ class PagesController < ApplicationController
   def index
     projects = Project.order_by_percentage
     project_success = Project.order_by_raised
-    @projects = Kaminari.paginate_array(projects).page(params[:page]).per(9)
-    @project_success = Kaminari.paginate_array(project_success).page(params[:page2]).per(6)
+    @projects = Kaminari.paginate_array(projects).page(params[:successful]).per(9)
+    @project_success = Kaminari.paginate_array(project_success).page(params[:featured]).per(6)
   end
 
   def home
