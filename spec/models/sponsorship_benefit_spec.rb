300c7eefa8da3c42f52981ff19e548a8a91657b9
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-22 10:10:29 -0300

    Improve views to display default benefits. [#69649614]

--- spec/models/sponsorship_benefit_spec.rb
+++ spec/models/sponsorship_benefit_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
 describe SponsorshipBenefit do
-	
+
 	it { should respond_to(:name) }
-	it { should respond_to(:sponsorship_level_id) } 
+	it { should respond_to(:sponsorship_level_id) }
 	it { should respond_to(:project_id, :status) }
 	it { should respond_to(:cost) }
+
+  describe "#default_plus_level" do
+    before(:each) do
+      @project = FactoryGirl.create(:project)
+      @benefits = FactoryGirl.create_list(:sponsorship_benefit, 5, \
+        project: @project, sponsorship_level_id: 1)
+      @default_benefit = SponsorshipBenefit::SPONSORSHIP_BENEFITS[1]
+    end
+
+    it { expect(SponsorshipBenefit.default_plus_level(@project, 1, 1)).to \
+      include @benefits.first }
+    it { expect(SponsorshipBenefit.default_plus_level(@project, 1, 1)).to \
+      include @default_benefit.first }
+    it { expect(SponsorshipBenefit.default_plus_level(@project, 1, 1)).to \
+      include @default_benefit.second }
+  end
 end
