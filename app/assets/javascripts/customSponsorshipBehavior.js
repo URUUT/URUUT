20f5d95e55c605242728a568ae1bda6b16a783ca
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-25 16:32:12 -0300

    Changes behavior for show the validator. [#70111778]

--- app/assets/javascripts/customSponsorshipBehavior.js
+++ app/assets/javascripts/customSponsorshipBehavior.js
@@ -1,18 +1,25 @@
 $('#next-link-to-assets').unbind();
+
+$("#confirm-changes").on('shown', function (e) {
+  e.preventDefault;
+  if ( !has_any_benefits() ) {
+    alert('You must select a benefit for each level.');
+    $('#confirm-changes').modal('hide');
+  } else {
+    $('#confirm-changes').modal('show');
+  }
+  return this;
+});
+
 $('#confirm-changes .btn-primary').click( function (e) {
   e.preventDefault();
-  if ( validate_benefits_check() ) {
-    $.ajax({
-      url: custom_level_url_path,
-      type: 'PUT',
-      data: $('.step-container[data-id="sponsorship"] form').serialize()
-    }).done(function(data) {
-      $('#confirm-changes').modal('hide');
-      window.location.href = '/projects/' + projectId + '/edit#assets';
-      window.location.reload(true);
-    });
-  } else {
+  $.ajax({
+    url: custom_level_url_path,
+    type: 'PUT',
+    data: $('.step-container[data-id="sponsorship"] form').serialize()
+  }).done(function(data) {
     $('#confirm-changes').modal('hide');
-    alert('You must select a benefit for each level.');
-  }
+    window.location.href = '/projects/' + projectId + '/edit#assets';
+    window.location.reload(true);
+  });
 });
