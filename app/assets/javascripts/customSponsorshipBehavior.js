3b6942ece5c275f3a5350c14fb48e25133102039
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-25 13:23:18 -0300

    Adds sponsorship level validation when it does not have any sponsorship benefit.

--- app/assets/javascripts/customSponsorshipBehavior.js
+++ app/assets/javascripts/customSponsorshipBehavior.js
@@ -1,13 +1,18 @@
 $('#next-link-to-assets').unbind();
 $('#confirm-changes .btn-primary').click( function (e) {
   e.preventDefault();
-  $.ajax({
-    url: custom_level_url_path,
-    type: 'PUT',
-    data: $('.step-container[data-id="sponsorship"] form').serialize()
-  }).done(function(data) {
+  if ( validate_benefits_check() ) {
+    $.ajax({
+      url: custom_level_url_path,
+      type: 'PUT',
+      data: $('.step-container[data-id="sponsorship"] form').serialize()
+    }).done(function(data) {
+      $('#confirm-changes').modal('hide');
+      window.location.href = '/projects/' + projectId + '/edit#assets';
+      window.location.reload(true);
+    });
+  } else {
     $('#confirm-changes').modal('hide');
-    window.location.href = '/projects/' + projectId + '/edit#assets';
-    window.location.reload(true);
-  });
+    alert('You must select a benefit for each level.');
+  }
 });
