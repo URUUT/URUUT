6974f00270a2ffedab63d108646facc9d1209ccc
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-29 11:26:39 -0300

    Correct validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,7 +18,7 @@ class Project < ActiveRecord::Base
   validates :organization, :organization_type, :address,
             :city, :state, :zip, :website, :facebook_page, :twitter_handle, :project_token,
             presence: true,:if => :sponsor_info
-  validates :organization_classification, presence: true, unless: "organization_type === 'Special Situation'"
+  validates :organization_classification, presence: true, unless: "organization_type === 'Special Situation'", if: :sponsor_info
 
   validates :project_title, :duration, :category, :title, :story, :about, presence: true, :if => :project_details
   validates :duration, numericality: { only_integer:true, greater_than: 0,
