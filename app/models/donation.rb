3186cedbb1b33936712f8e681c427ec0c597d6fb
Ezekiel <ezerivadeneiral@gmail.com>
2014-05-05 12:39:27 -0300

    Add tax report to apply charge method. [#62338326]
       Missing report on method.
       Refactor exceptions.

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -183,44 +183,30 @@ class Donation < ActiveRecord::Base
           project.project_token
         )
         update_column(:approved, true)
+        user.generate_tax_report(project)
         return true
       rescue Stripe::CardError => e
         # Since it's a decline, Stripe::CardError will be caught
         body = e.json_body
         err  = body[:error]
 
-        puts "Status is: #{e.http_status}"
-        puts "Type is: #{err[:type]}"
-        puts "Code is: #{err[:code]}"
+        Rails.logger.error "Status is: #{e.http_status}"
+        Rails.logger.error "Type is: #{err[:type]}"
+        Rails.logger.error "Code is: #{err[:code]}"
         # param is '' in this case
-        puts "Param is: #{err[:param]}"
-        puts "Message is: #{err[:message]}"
-        return false
-      rescue Stripe::InvalidRequestError => e
-        # Invalid parameters were supplied to Stripe's API
-        puts e
-        return false
-      rescue Stripe::AuthenticationError => e
-        # Authentication with Stripe's API failed
-        # (maybe you changed API keys recently)
-        puts e
-        return false
-      rescue Stripe::APIConnectionError => e
-        # Network communication with Stripe failed
-        puts e
-        return false
-      rescue Stripe::StripeError => e
-        # Display a very generic error to the user, and maybe send
-        # yourself an email
-        puts e
-        return false
+        Rails.logger.error "Param is: #{err[:param]}"
+        Rails.logger.error "Message is: #{err[:message]}"
+      rescue Stripe::InvalidRequestError, Stripe::AuthenticationError,
+             Stripe::APIConnectionError, Stripe::StripeError => e
+        Rails.logger.error e
       rescue => e
         # Something else happened, completely unrelated to Stripe
-        puts e
-        return false
+        Rails.logger.error e
       end
+      false
+    else
+      true
     end
-    true
   end
 
   private
