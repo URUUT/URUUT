529115b8f947ab2b7b928b9d86021284306d8e85
amancke <alejo@bandofcoders.com>
2014-05-05 21:02:17 -0300

    [#59929592] Change emails from field
    Sponsor Thank you email.

--- app/mailers/sponsor_mailer.rb
+++ app/mailers/sponsor_mailer.rb
@@ -66,8 +66,9 @@ class SponsorMailer < ActionMailer::Base
     @project_facebook = project.facebook_page
     @project_twitter = project.twitter_handle
     @project_id = project.id
+    @sponsor_email = sponsor.email
 
-    mail to: @email, subject: "Thank You, Sponsor!"
+    mail from: @sponsor_email, to: @email, subject: "Thank You, Sponsor!"
   end
 
   def send_sponsor_report
