6fe3e405ae411dca658598e41bfd7c1a65dbe8b8
Ezekiel <ezerivadeneiral@gmail.com>
2014-05-06 12:57:44 -0300

    Add user creation spec for plus and fee users. [#66970236]

--- spec/features/user_creation_spec.rb
+++ spec/features/user_creation_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+
 feature 'User creation' do
   before do
     @user = FactoryGirl.build(:user)
@@ -11,10 +12,8 @@ feature 'User creation' do
     expect(current_path).to eql choose_plan_pages_path
   end
 
-  scenario "User choise a basic plan", js: true do
-    WebMock.allow_net_connect!
+  scenario "User clicks a basic plan and display a popup" do
     visit choose_plan_pages_path
-
     find('[data-plan-type="basic"]').click
     sleep(0.5)
     fill_in 'marketing_info[first_name]', with: @user.first_name
@@ -23,25 +22,23 @@ feature 'User creation' do
     click_button 'Next ►'
 
     expect(page).to have_content('Uruut Basic')
+  end
 
-    fill_in 'user[organization]',         with: 'Custom Organization'
-    fill_in 'user[telephone]',            with: '123123123123'
-    fill_in 'user[password]',             with: @user.password
-    fill_in 'user[password_confirmation]', with: @user.password_confirmation
+  scenario "User choise a basic plan", js: true do
+    WebMock.allow_net_connect!
+    visit new_user_registration_path(
+      full_registration: 1,
+      sign_up_plan: :basic,
+      marketing_info_id: 19
+    )
+
+    fill_basic_sign_up
 
     click_button('Continue')
 
     expect(page).to have_content('Uruut Basic')
 
-    fill_in 'credit_card[name]',          with: @user.first_name
-    fill_in 'credit_card[number]',        with: '4242424242424242'
-    fill_in 'credit_card[cvc]',           with: '123'
-    fill_in 'credit_card[exp_month]',     with: '12'
-    fill_in 'credit_card[exp_year]',      with: 1.year.from_now.year
-    fill_in 'credit_card[billing_address]', with: 'St fake'
-    fill_in 'credit_card[city]',          with: 'Fake City'
-    select 'AL',                          from: 'credit_card[state]'
-    fill_in 'credit_card[zip_code]',      with: '3000'
+    fill_basic_credit_card
 
     click_button('Submit ►')
 
@@ -53,5 +50,72 @@ feature 'User creation' do
     expect(User.last.membership.kind).to eql 'basic'
   end
 
+  scenario "User choise a plus plan", js: true do
+    WebMock.allow_net_connect!
+    visit new_user_registration_path(
+      full_registration: 1,
+      sign_up_plan: :plus,
+      marketing_info_id: 20
+    )
 
+    fill_basic_sign_up
+
+    click_button('Continue')
+
+    expect(page).to have_content('Uruut Plus')
+
+    fill_basic_credit_card
+
+    click_button('Submit ►')
+
+    expect(page).to have_content 'Thank You! Your Uruut Account is Now Active.'
+    WebMock.disable_net_connect!(allow_localhost: true)
+
+    expect(User.last.first_name).to eql @user.first_name
+    expect(User.last.organization).to eql 'Custom Organization'
+    expect(User.last.membership.kind).to eql 'plus'
+  end
+
+  scenario "User choise a fee plan" do
+    WebMock.allow_net_connect!
+    visit new_user_registration_path(
+      full_registration: 1,
+      sign_up_plan: :fee,
+      marketing_info_id: 21
+    )
+
+    fill_basic_sign_up
+
+    click_button('Finish')
+
+    expect(page).to have_content 'Thank You! Your Uruut Account is Now Active.'
+    WebMock.disable_net_connect!(allow_localhost: true)
+
+    expect(User.last.first_name).to eql @user.first_name
+    expect(User.last.organization).to eql 'Custom Organization'
+    expect(User.last.membership.kind).to eql 'fee'
+  end
+
+end
+
+def fill_basic_sign_up
+  fill_in 'user[first_name]',           with: @user.first_name
+  fill_in 'user[last_name]',            with: @user.last_name
+  fill_in 'user[email]',                with: @user.email
+  fill_in 'user[organization]',         with: 'Custom Organization'
+  fill_in 'user[telephone]',            with: '123123123123'
+  fill_in 'user[password]',             with: @user.password
+  fill_in 'user[password_confirmation]', with: @user.password_confirmation
+end
+
+def fill_basic_credit_card
+  fill_in 'credit_card[name]',          with: @user.first_name
+  fill_in 'credit_card[number]',        with: '4242424242424242'
+  fill_in 'credit_card[cvc]',           with: '123'
+  fill_in 'credit_card[exp_month]',     with: '12'
+  fill_in 'credit_card[exp_year]',      with: 1.year.from_now.year
+  fill_in 'credit_card[billing_address]', with: 'St fake'
+  fill_in 'credit_card[city]',          with: 'Fake City'
+  select 'AL',                          from: 'credit_card[state]'
+  fill_in 'credit_card[zip_code]',      with: '3000'
 end
