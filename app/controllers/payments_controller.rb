5744acfa20c273fae275f9c363333a20beaf7d01
Chad Bartels <chad.bartels@gmail.com>
2014-05-14 16:27:55 -0400

    moved sponsor mailers to payment controller...sigh

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,6 +6,9 @@ class PaymentsController < ApplicationController
     project_sponsor = project.project_sponsors.unscoped.find_by_sponsor_id(sponsor.id)
     project_sponsor.update_attributes!(confirmed: true)
 
+    SponsorMailer.delay.new_sponsor(sponsor)
+    SponsorMailer.delay.sponsor_thank_you(sponsor.id, sponsor.email)
+
     redirect_to thank_you_for_sponsor_url(project.id, sponsor.id)
 
   end
