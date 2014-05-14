404982e76c2d6af4242078abb4c85140efbbc978
Ezekiel <ezerivadeneiral@gmail.com>
2014-05-14 13:35:52 -0300

    Add error message when a invalid coupon is added. [#66709326]

--- app/services/gateway/coupon_service.rb
+++ app/services/gateway/coupon_service.rb
@@ -1,7 +1,7 @@
 require 'stripe'
 
 class Gateway::CouponService < Gateway::BaseService
-  
+
   def create(coupon)
     return true if coupon.blank?
     return false unless retrieve(coupon)
@@ -9,22 +9,23 @@ class Gateway::CouponService < Gateway::BaseService
     find_customer
     customer.coupon = coupon
     customer.save
-      
+
     user.coupon_stripe_token = coupon
     user.save
     rescue => e
       Rails.logger.error e
     end
   end
-  
+
   private
   def retrieve(coupon)
     begin
     Stripe::Coupon.retrieve(coupon)
     rescue => e
+      user.errors.add(:coupon_stripe_token, "No such coupon #{coupon}")
       Rails.logger.error e
       false
     end
   end
-  
+
 end
