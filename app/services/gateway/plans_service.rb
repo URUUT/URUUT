e4b40f4155addaaa3a4d05b91d19fc1812cd7f15
Ezekiel <ezerivadeneiral@gmail.com>
2014-05-29 16:10:12 -0300

    Solve syntax error.

--- app/services/gateway/plans_service.rb
+++ app/services/gateway/plans_service.rb
@@ -29,7 +29,6 @@ class Gateway::PlansService < Gateway::BaseService
       return true if coupon.valid
       user.errors.add(:coupon_stripe_token, "Invalid coupon. #{@coupon}")
       return false
-      end
       rescue => e
         user.errors.add(:coupon_stripe_token, "No such coupon #{@coupon}")
         Rails.logger.error e
