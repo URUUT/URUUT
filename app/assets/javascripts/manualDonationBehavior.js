dbafc2d2a24dc6a0c8b4d55b0d11bdb6f7226861
Ezekiel <ezerivadeneiral@gmail.com>
2014-06-02 11:31:31 -0300

    Add ManualDonations to workflow. [#71848298]

---
+++ app/assets/javascripts/manualDonationBehavior.js
@@ -0,0 +1,24 @@
+$('#donation-submit').on('click', function (e) {
+  e.preventDefault();
+  var url = this.form.getAttribute('action'),
+      data = $(this.form).serialize();
+  var request = $.post(url, data)
+
+  request.done(function(data, status, xhr) {
+    console.log('Manual Donation was added successfully.');
+  });
+
+  request.fail(function(response, status, xhr) {
+    var errors = JSON.parse(response.responseText);
+    processErrors(errors, 'manual_donation');
+  });
+})
+
+function processErrors(errors, form) {
+  for ( field in errors ) {
+    el = ['#', form, '_', field].join('');
+    $("#new_" + form).find(el).parents('.field').find('.error')
+      .css('display', 'inline').text( errors[field].join(',') );
+    $('#new_' + form).find(el).focus();
+  }
+}
