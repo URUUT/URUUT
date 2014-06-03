f6e5824eaf54c6d424914d9b216dd61f21850e92
Ezekiel <ezerivadeneiral@gmail.com>
2014-06-03 15:18:08 -0300

    Add jquery validator to manual donations. [#71848298]

--- app/assets/javascripts/manualDonationBehavior.js
+++ app/assets/javascripts/manualDonationBehavior.js
@@ -1,26 +1,40 @@
+$('#new_manual_donation').validate({
+  rules: {
+    "manual_donation[first_name]": { required: true },
+    "manual_donation[last_name]": { required: true },
+    "manual_donation[email]": { required: true, email: true },
+    "manual_donation[amount]": { required: true, number: true, min: 1 }
+  },
+  errorPlacement: function(error, element) {
+    error.appendTo( element.parent().find(".error-container") );
+  }
+});
+
 $('#donation-submit').on('click', function (e) {
   e.preventDefault();
   var url = this.form.getAttribute('action'),
       data = $(this.form).serialize(),
       old_value = this.value,
       that = this;
-  this.value = 'Loading'
 
-  var request = $.ajax({
-    type: this.form.getAttribute('method'),
-    data: data,
-    url: url
-  })
+  if ( $('#new_manual_donation').valid() ) {
+    this.value = 'Loading'
+    var request = $.ajax({
+      type: this.form.getAttribute('method'),
+      data: data,
+      url: url
+    })
 
-  request.done(function(data, status, xhr) {
-    console.log('Manual Donation was added successfully.');
-  });
+    request.done(function(data, status, xhr) {
+      console.log('Manual Donation was added successfully.');
+    });
 
-  request.fail(function(response, status, xhr) {
-    var errors = JSON.parse(response.responseText);
-    processErrors(errors, 'manual_donation');
-    that.value = old_value
-  });
+    request.fail(function(response, status, xhr) {
+      var errors = JSON.parse(response.responseText);
+      processErrors(errors, 'manual_donation');
+      that.value = old_value
+    });
+  }
 })
 
 function processErrors(errors, form) {
