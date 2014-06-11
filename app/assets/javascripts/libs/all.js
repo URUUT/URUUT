f4b946406cb3feda3f5c0be8bf66342c10e531be
Ezekiel <ezerivadeneiral@gmail.com>
2014-06-11 16:37:47 -0300

    Improve landing layout. [#59929562] [#59997342]

---
+++ app/assets/javascripts/libs/all.js
@@ -0,0 +1,3 @@
+//= require ./google_analytics
+//= require ./facebook_initializr
+//= require ./user_voice
---
+++ app/assets/javascripts/libs/facebook_initializr.js
@@ -0,0 +1,18 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : Settings.facebook.api_key,
+    channelUrl : '',
+    status     : true,
+    xfbml      : true
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
