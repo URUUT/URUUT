1dc4336503de00aeaf58d6b32ab4fd18eef7b555
Ezekiel <ezerivadeneiral@gmail.com>
2014-06-16 11:24:13 -0300

    Improve project filters scripts. [#59929562]

--- app/assets/javascripts/behaviors/project_filters_behaviors.js
+++ app/assets/javascripts/behaviors/project_filters_behaviors.js
@@ -12,3 +12,34 @@ $("#category, #location").on("change", function(){
     $(".loader").fadeOut("fast");
   });
 });
+
+function truncate_location_text() {
+  if ($("#location :selected").text().length > 17){
+    $("#location :selected").text(truncText($("#location :selected").text(), 15, "..."));
+  }
+};
+
+function truncate_category_text() {
+  if ($("#category :selected").text().length > 17){
+    $("#category :selected").text(truncText($("#category :selected").text(), 15, "..."));
+  }
+};
+
+truncate_category_text();
+truncate_location_text();
+
+$("#category").on('change',function(){
+  var length = $("#category option").length;
+  for (var i = 1; i < length; i++){
+    $("#category option")[i].text = $("#category option")[i].attributes["data-item"].value
+  }
+  truncate_category_text();
+});
+
+$("#location").on('change',function(){
+  var length = $("#location option").length;
+  for (var i = 1; i < length; i++){
+    $("#location option")[i].text = $("#location option")[i].attributes["data-item"].value
+  }
+  truncate_location_text();
+});
