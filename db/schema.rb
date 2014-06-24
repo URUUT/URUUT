3696492a7c23d901c40b3cf9aa11606089ff246c
Chad Bartels <chad.bartels@gmail.com>
2014-06-24 18:34:08 -0400

    pushing papertrail to dev

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(:version => 20140624135030) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "admin_users", :force => true do |t|
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
     t.string   "email",                  :default => "",       :null => false
     t.string   "encrypted_password",     :default => "",       :null => false
     t.string   "reset_password_token"
@@ -46,6 +44,8 @@ ActiveRecord::Schema.define(:version => 20140624135030) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
     t.string   "role",                   :default => "member"
   end
 
@@ -64,9 +64,11 @@ ActiveRecord::Schema.define(:version => 20140624135030) do
   add_index "badges_sashes", ["sash_id"], :name => "index_badges_sashes_on_sash_id"
 
   create_table "comments", :force => true do |t|
-    t.text    "body"
-    t.integer "post_id"
-    t.integer "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.text     "body"
+    t.integer  "post_id"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
@@ -317,12 +319,12 @@ ActiveRecord::Schema.define(:version => 20140624135030) do
   end
 
   create_table "posts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "title"
     t.text     "body"
-    t.integer  "project_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
   add_index "posts", ["project_id"], :name => "index_posts_on_project_id"
@@ -521,6 +523,7 @@ ActiveRecord::Schema.define(:version => 20140624135030) do
   end
 
   add_index "tax_reports", ["project_id"], :name => "index_tax_reports_on_project_id"
+  add_index "tax_reports", ["user_id"], :name => "index_tax_reports_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",                           :null => false
